6961bde57dc1ea5d9f49d518030ad29174c7f7f5
SheepEatLion <gowjr207@gmail.com>
2022-01-07 19:23:27 +0900

    feat 게시글 구현
    - 보드 페이지 임시 구현
    - 포스트 임시 구현

---
+++ src/Board.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="board">
+    <Post />
+    <Paging />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Post from './components/post.vue'
+import Paging from './components/paging.vue'
+import Footer from './components/footer.vue'
+
+export default {
+  name: 'Board',
+  components: {
+    Post,
+    Paging,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+    font-family: 'YeolrinMyeongjo-Medium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/YeolrinMyeongjo-Medium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+#app {
+  font-family: 'YeolrinMyeongjo-Medium';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
